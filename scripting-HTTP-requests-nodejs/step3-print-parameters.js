7f3b8304fd84ea56f07bccdf576bde23ef286dd8
Ryan Kendrick <rpkendrick@gmail.com>
2018-10-29 17:25:39 +0000

    completed step 3: print with parameters

---
+++ scripting-HTTP-requests-nodejs/step3-print-parameters.js
@@ -0,0 +1,40 @@
+var https = require('https');
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+function getAndPrintHTML (options) {
+
+
+
+    // notice that https.get takes a callback with one parameter -
+    // response, which is a Stream that represents the HTTP response
+    https.get(options, function (response) {
+
+      var dataString = '';
+
+      // set encoding of received data to UTF-8
+      response.setEncoding('utf8');
+
+      // the callback is invoked when a `data` chunk is received
+      response.on('data', function (data) {
+        console.log('Chunk Received. Length:', data.length)
+        dataString += data;
+
+      });
+
+      // the callback is invoked when all of the data has been received
+      // (the `end` of the stream)
+      response.on('end', function() {
+        console.log('Response stream complete.');
+        console.log(dataString);
+      });
+
+});
+
+}
+
+getAndPrintHTML(requestOptions);
